ebe0956373e76d4bfffdb0b957e1360bdc966c1e
Adarsh R <42464891+adarsh-qclw@users.noreply.github.com>
2020-10-22 11:44:50 +0530

    Create time-conversion.java

---
+++ problem solving/time-conversion.java
@@ -0,0 +1,33 @@
+/*
+ * [Time Conversion](https://www.hackerrank.com/challenges/time-conversion/problem)     
+ */
+ 
+package com.javaaid.hackerrank.solutions.generalprogramming.basicprogramming;
+
+
+public class TimeConversion {
+	static String timeConversion(String s) {
+		String[] str = s.split(":");
+		int hour = Integer.parseInt(str[0]);
+
+		String min = str[1];
+		String secPeriod = str[2];
+		String sec = str[2].substring(0, secPeriod.length() - 2);
+		String period = str[2].substring(secPeriod.length() - 2, secPeriod.length());
+
+		String newTimeINString = "";
+
+		if ((0 <= hour && hour < 12) && (period.equalsIgnoreCase("AM"))) {
+			newTimeINString = String.format("%02d", hour) + ":" + min + ":" + sec;
+		} else if ((0 <= hour && hour < 12) && period.equalsIgnoreCase("PM")) {
+			newTimeINString = (12 + hour) + ":" + min + ":" + sec;
+
+		} else if ((hour == 12) && (period.equalsIgnoreCase("AM"))) {
+			newTimeINString = "00" + ":" + min + ":" + sec;
+		} else if ((hour == 12) && (period.equalsIgnoreCase("PM"))) {
+			newTimeINString = hour + ":" + min + ":" + sec;
+		}
+		return newTimeINString;
+	}
+
+}
